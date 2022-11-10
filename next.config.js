db288e42ed3e086c88c7d255642f05b0d4881976
AkashS <akash.s@krishaweb.com>
2022-11-10 10:42:57 +0530

    nextconfig

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
- 
-}
-
-module.exports = nextConfig
+  experimental: {
+    forceSwcTransforms: true,
+  },
+};
