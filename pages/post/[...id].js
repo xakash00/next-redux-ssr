1e8d572d0647d42fe1d8b834f5b60fe9df7d176a
AkashS <akash.s@krishaweb.com>
2022-11-11 10:51:48 +0530

    gssp

--- pages/post/[...id].js
+++ pages/post/[...id].js
@@ -14,24 +14,22 @@ const Post = () => {
   return (
     <>
       <HeaderLayout>
-        {/* <SinglePost /> */}
-
-        <h1>Post</h1>
+        <SinglePost />
       </HeaderLayout>
     </>
   );
 };
 
-// export const getServerSideProps = wrapper.getServerSideProps(async (ctx) => {
-//   console.log("inside getStaticProps.. store.dispatch");
-//   console.log(ctx, "aslkiub");
-//   if (!ctx.store.getState().placeholderData) {
-//     ctx.store.dispatch(singlePostAction(ctx.query.id));
-//     ctx.store.dispatch(fetchCommentsAction(ctx.query.id));
-//     console.log("ending saga on server now...");
-//     ctx.store.dispatch(END);
-//   }
-//   await ctx.store.sagaTask.toPromise();
-// });
+export const getServerSideProps = wrapper.getServerSideProps(async (ctx) => {
+  console.log("inside getStaticProps.. store.dispatch");
+  console.log(ctx, "aslkiub");
+  if (!ctx.store.getState().placeholderData) {
+    ctx.store.dispatch(singlePostAction(ctx.query.id));
+    ctx.store.dispatch(fetchCommentsAction(ctx.query.id));
+    console.log("ending saga on server now...");
+    ctx.store.dispatch(END);
+  }
+  await ctx.store.sagaTask.toPromise();
+});
 
 export default Post;
