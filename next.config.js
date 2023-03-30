26d86340bcf24a852db93452de2754ad1fb33500
AkashS <akash.s@krishaweb.com>
2023-03-30 16:03:37 +0530

    security headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,36 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  swcMinify: true,
+  async headers(){
+    [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value:"default-src 'self'"
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: "camera=(); battery=(self); geolocation=(); microphone=()",
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  } 
   // experimental: {
   //   forceSwcTransforms: true,
   //   nextScriptWorkers: true,
